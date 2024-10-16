e13ec8d9ed6d4bc3202775962f796415b806a809
Renee Li <renee.li@fivetran.com>
2024-10-16 14:53:43 -0400

    add is_intraday coalesce to true

--- models/staging/stg_ga4_export__event.sql
+++ models/staging/stg_ga4_export__event.sql
@@ -25,8 +25,8 @@ fields as (
 
 ),
 
-final_pre as (
-    
+final as (
+
     select
         cast(_fivetran_id as {{ dbt.type_string() }}) as fivetran_id,
         {{ dbt.concat(["user_pseudo_id","'_'","event_timestamp","'_'","event_name","'_'","bundle_sequence_id"]) }} as event_id,
@@ -70,17 +70,9 @@ final_pre as (
 
     from fields
 
-),
-
-exclude_intraday as (
-
-    select
-        *
-    from final_pre
-    where is_intraday = false
-
 )
 
 select
     *
-from exclude_intraday
+from final
+where not coalesce(is_intraday,true)
