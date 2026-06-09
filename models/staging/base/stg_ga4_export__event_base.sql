484b526add44f2d6a4c821b37a5aebc973e58c05
Avinash Kunnath <avinash.kunnath@fivetran.com>
2026-06-09 15:02:11 -0400

    Source relation

--- models/staging/base/stg_ga4_export__event_base.sql
+++ models/staging/base/stg_ga4_export__event_base.sql
@@ -1,8 +1,10 @@
+{% if var('ga4_export_union_schemas', []) | length > 0 or var('ga4_export_union_databases', []) | length > 0 %}
+
 {{
     fivetran_utils.union_data(
-        table_identifier='event', 
-        database_variable='ga4_export_database', 
-        schema_variable='ga4_export_schema', 
+        table_identifier='event',
+        database_variable='ga4_export_database',
+        schema_variable='ga4_export_schema',
         default_database=target.database,
         default_schema='ga4_export',
         default_variable='event',
@@ -10,3 +12,15 @@
         union_database_variable='ga4_export_union_databases'
     )
 }}
+
+{% else %}
+
+{{
+    fivetran_utils.union_connections(
+        connection_dictionary='ga4_export_sources',
+        single_source_name='ga4_export',
+        single_table_name='event'
+    )
+}}
+
+{% endif %}
